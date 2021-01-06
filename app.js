f41630df85bbcf242d93aa6c9845bf8f7923d985
manci <javiercaliescali@gmail.com>
2021-01-06 12:17:33 -0500

    obtener datos en console de api

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+const axios = require('axios');
+
 const argv = require('yargs').options({
   direccion: {
     alias: 'd',
@@ -5,5 +7,23 @@ const argv = require('yargs').options({
     demand: true
   }
 }).argv;
+
+const encodeUlr = encodeURI(argv.direccion); // convierte a caracteres seguros "new york" cuando hay espacios o así
+console.log(encodeUlr); // se puede agregar la encodeUlr como parametro de baseURL
+
+const instance = axios.create({
+  baseURL: 'https://videosapi-gon.herokuapp.com/api/v1/videos/',
+  headers: {'x-rapidapi-key': '4a0415d61fmshb29aee0f23eef58p1eb7f1jsnaec133322bc6'}
+});
+
+instance.get()
+  .then(resp => {
+    console.log(resp.data.data[0]);
+  })
+  .catch(err => {
+    console.log('Error', err);
+  });
+
+
 
 // console.log(argv.direccion);
